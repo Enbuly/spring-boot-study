aea1d9bdb8dd28f40353004c70f48568fd8e297c
zzy <120157229@qq.com>
2018-12-28 03:10:19 +0800

    commit redis

---
+++ web/common/src/main/java/com/example/config/LettuceRedisConfig.java
@@ -0,0 +1,43 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * RedisLettuce配置
+ *
+ * @author zhangzhenyan
+ * @since 2018-12-28
+ */
+@Configuration
+public class LettuceRedisConfig {
+
+    @Autowired
+    private LettuceConnectionFactory lettuceConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisCacheTemplate() {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+
+}
--- web/controller/src/test/java/com/example/test/SpringBootJunitTest.java
+++ web/controller/src/test/java/com/example/test/SpringBootJunitTest.java
@@ -3,10 +3,12 @@ package com.example.test;
 import com.example.api.UserService;
 import com.example.mapper.UserMapping;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
@@ -20,6 +22,9 @@ public class SpringBootJunitTest {
     @Autowired
     private UserMapping userMapping;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void testFirst() {
         System.out.println(userService.getPassword("zzy"));
@@ -28,5 +33,13 @@ public class SpringBootJunitTest {
     @Test
     public void testSecond() {
         System.out.println(userMapping.selectPasswordByUserName("zzy"));
+    }
+
+    @Test
+    public void redisTest() {
+        String key = StringUtils.join(new String[]{"user", "token"}, ":");
+        String value = "0171826834da554b43d2c72bb1767c7898f27bf91775463e2b3b4e0f3806e0255d6e52ca286b";
+        stringRedisTemplate.opsForValue().set(key, value);
+        System.out.println(stringRedisTemplate.opsForValue().get(key));
     }
 }
