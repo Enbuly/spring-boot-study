a31ae1129d30ace46dd55939f1748212ca5cc00b
zhangzhenyan <120157229@qq.com>
2019-05-07 16:15:47 +0800

    提交定时任务代码

---
+++ web/common/src/main/java/com/example/config/StaticScheduleTask.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@Configuration
+@EnableScheduling
+public class StaticScheduleTask {
+
+    private Logger log = LoggerFactory.getLogger(StaticScheduleTask.class);
+
+    //每隔5秒执行一次：*/5 * * * * ?
+    //每隔5分执行一次：0 */5 * * * ?
+    //每天半夜12点执行一次：0 0 0 * * ?
+    //每天中午十二点执行一次：0 0 12 * * ?
+    @Scheduled(cron = "0 */5 * * * ?")
+    private void configureTasks() {
+        log.info("执行静态定时任务时间: " + LocalDateTime.now());
+    }
+
+}
