a45a94716f8d809189ced7ac09048d254dc5cd94
zhangzhenyan <120157229@qq.com>
2019-05-29 10:51:18 +0800

    提交xss相关

--- web/common/src/main/java/com/example/wrapper/XssHttpServletRequestWrapper.java
+++ web/common/src/main/java/com/example/wrapper/XssHttpServletRequestWrapper.java
@@ -18,6 +18,7 @@ import java.util.Map;
  * @author zhangzhenyan
  * @since 2019-05-27
  **/
+@SuppressWarnings("unchecked")
 public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
     private HttpServletRequest orgRequest;
@@ -27,15 +28,8 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         orgRequest = request;
     }
 
-    public static HttpServletRequest getOrgRequest(HttpServletRequest req) {
-        if (req instanceof XssHttpServletRequestWrapper) {
-            return ((XssHttpServletRequestWrapper) req).getOrgRequest();
-        }
-        return req;
-    }
-
     @Override
-    public ServletInputStream getInputStream() throws IOException {//get
+    public ServletInputStream getInputStream() throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(orgRequest.getInputStream()));
         String line = br.readLine();
@@ -64,10 +58,10 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     public Map getParameterMap() {
         Map map = super.getParameterMap();
         // 返回值Map
-        Map<String, String> returnMap = new HashMap<String, String>();
+        Map<String, String> returnMap = new HashMap<>();
         Iterator entries = map.entrySet().iterator();
         Map.Entry entry;
-        String name = "";
+        String name;
         String value = "";
         while (entries.hasNext()) {
             entry = (Map.Entry) entries.next();
@@ -77,8 +71,8 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
                 value = "";
             } else if (valueObj instanceof String[]) {
                 String[] values = (String[]) valueObj;
-                for (int i = 0; i < values.length; i++) {
-                    value = values[i] + ",";
+                for (String v : values) {
+                    value = v + ",";
                 }
                 value = value.substring(0, value.length() - 1);
             } else {
@@ -100,23 +94,14 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         return arr;
     }
 
-    private HttpServletRequest getOrgRequest() {
-        return orgRequest;
-    }
-
     private String clean(String content) {
-        String result = JsoUpUtil.clean(content);
-        return result;
+        return JsoUpUtil.clean(content);
     }
 
     private class WrappedServletInputStream extends ServletInputStream {
         private InputStream stream;
 
         WrappedServletInputStream(InputStream stream) {
-            this.stream = stream;
-        }
-
-        public void setStream(InputStream stream) {
             this.stream = stream;
         }
 
