9a35a25608f38ec643cf3e45770afbd04cd73edc
zhangzhenyan <120157229@qq.com>
2019-06-12 09:09:17 +0800

    提交更新

--- web/common/src/main/java/com/example/config/ThreadPoolTaskExecutorConfig.java
+++ web/common/src/main/java/com/example/config/ThreadPoolTaskExecutorConfig.java
@@ -46,12 +46,9 @@ public class ThreadPoolTaskExecutorConfig {
         executor.setMaxPoolSize(maxPoolSize);
         executor.setQueueCapacity(queueCapacity);
         executor.setThreadNamePrefix(namePrefix);
-        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-
-        //执行初始化
         executor.initialize();
+
         return executor;
     }
 }
