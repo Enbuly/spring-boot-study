47de800b3f0f8fd2f1ccde4522dcd034840acecc
zhangzhenyan <120157229@qq.com>
2019-06-18 15:41:26 +0800

    提交更新

--- web/common/src/main/java/com/example/wrapper/XssHttpServletRequestWrapper.java
+++ web/common/src/main/java/com/example/wrapper/XssHttpServletRequestWrapper.java
@@ -3,8 +3,11 @@ package com.example.wrapper;
 import com.example.util.JsoupUtil;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
 
 /**
  * XssHttpServletRequestWrapper
@@ -16,9 +19,12 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
     private boolean isIncludeRichText;
 
+    private HttpServletRequest httpServletRequest;
+
     public XssHttpServletRequestWrapper(HttpServletRequest request, boolean isIncludeRichText) {
         super(request);
         this.isIncludeRichText = isIncludeRichText;
+        httpServletRequest = request;
     }
 
     /**
@@ -57,6 +63,20 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         return arr;
     }
 
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(httpServletRequest.getInputStream()));
+        String line = br.readLine();
+        StringBuilder result = new StringBuilder();
+        while (line != null) {
+            result.append(JsoupUtil.clean(line));
+            line = br.readLine();
+        }
+
+        return new WrappedServletInputStream(new ByteArrayInputStream(result.toString().getBytes()));
+    }
+
     private String cleanIllegalChars(String name) {
         if (!isIncludeRichText) {
             return super.getParameter(name);
@@ -68,4 +88,33 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         }
         return value;
     }
+
+    private class WrappedServletInputStream extends ServletInputStream {
+
+        private InputStream stream;
+
+        WrappedServletInputStream(InputStream stream) {
+            this.stream = stream;
+        }
+
+        @Override
+        public int read() throws IOException {
+            return stream.read();
+        }
+
+        @Override
+        public boolean isFinished() {
+            return true;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+        }
+    }
+
 }
