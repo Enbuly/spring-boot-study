727792f87daf3e4048039f657d0d8db6bae7111e
zhangzhenyan <120157229@qq.com>
2019-06-18 09:40:12 +0800

    提交更新

--- web/common/src/main/java/com/example/exception/handler/GlobalExceptionHandler.java
+++ web/common/src/main/java/com/example/exception/handler/GlobalExceptionHandler.java
@@ -36,7 +36,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResultVo defaultExceptionHandler(HttpServletRequest request, Exception ex) {
         log.error("DefaultException Handler---Host {} invoke url {} ERROR: {}", request.getRemoteHost(), request.getRequestURL(), ex);
-        return ResultVo.error("system error");
+        return ResultVo.error("system busy");
     }
 
 }
