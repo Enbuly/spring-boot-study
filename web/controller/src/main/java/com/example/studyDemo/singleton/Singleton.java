440d05b38727b85a1cd20a40bdb79038ebd30ea3
zhangzhenyan <120157229@qq.com>
2019-07-15 17:14:30 +0800

    更新doc

---
+++ web/controller/src/main/java/com/example/studyDemo/singleton/Singleton.java
@@ -0,0 +1,24 @@
+package com.example.studyDemo.singleton;
+
+/**
+ * Singleton
+ *
+ * @author lazy cat
+ * 说明：
+ * 当Singleton类被加载时，静态内部类SingletonHolder没有被加载进内存。
+ * 只有当调用getUniqueInstance()方法从而触发SingletonHolder.INSTANCE时
+ * SingletonHolder才会被加载，此时初始化INSTANCE实例，并且JVM能确保INSTANCE只被实例化一次。
+ * 这种方式不仅具有延迟初始化的好处，而且由JVM提供了对线程安全的支持。
+ **/
+public class Singleton {
+    private Singleton() {
+    }
+
+    public static Singleton getUniqueInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+}
---
+++ web/controller/src/main/java/com/example/studyDemo/singleton/Singleton2.java
@@ -0,0 +1,25 @@
+package com.example.studyDemo.singleton;
+
+/**
+ * Singleton2
+ * <p>
+ * double check lock
+ **/
+public class Singleton2 {
+
+    private volatile static Singleton2 uniqueInstance;
+
+    private Singleton2() {
+    }
+
+    public static Singleton2 getUniqueInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton2();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
