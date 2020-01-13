ec62c29d4b9e3fcebc69938579e4400e0152df9c
zhangzhenyan <120157229@qq.com>
2020-01-13 09:44:55 +0800

    提交代码

--- web/common/src/main/java/com/example/annotation/secret/SecretBody.java
+++ web/common/src/main/java/com/example/annotation/secret/SecretBody.java
@@ -1,9 +1,6 @@
 package com.example.annotation.secret;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * 加密解密http请求
@@ -13,5 +10,7 @@ import java.lang.annotation.Target;
  **/
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+@Inherited
 public @interface SecretBody {
 }
