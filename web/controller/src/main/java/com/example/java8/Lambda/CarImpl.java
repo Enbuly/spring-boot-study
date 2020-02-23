fdf986aca9a4db2c3603642d47057cc74f52a3b8
zhangzhenyan <120157229@qq.com>
2020-02-23 15:04:44 +0800

    提交代码

--- web/controller/src/main/java/com/example/java8/Lambda/CarImpl.java
+++ web/controller/src/main/java/com/example/java8/Lambda/CarImpl.java
@@ -10,12 +10,14 @@ public class CarImpl implements Car {
 
     public static void main(String[] args) {
 
-        // 1.传统方式 需要new接口的实现类来完成对接口的调用
         Car car1 = new CarImpl();
         car1.drive();
 
         Car car4 = () -> System.out.println("Drive Ferrari");
         car4.drive();
+
+        CarPro carPro = (String a, String b) -> a + b;
+        System.out.println(carPro.run("zzy", " is good"));
     }
 
     @Override
---
+++ web/controller/src/main/java/com/example/java8/Lambda/CarPro.java
@@ -0,0 +1,12 @@
+package com.example.java8.Lambda;
+
+/**
+ * CarPro
+ *
+ * @author lazy cat
+ * @since 2020-02-23
+ **/
+public interface CarPro {
+
+    String run(String a, String b);
+}
