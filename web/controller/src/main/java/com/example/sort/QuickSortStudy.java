fb21d044365ee15300655ee18a96441076befc78
zhangzhenyan <120157229@qq.com>
2020-04-18 11:20:38 +0800

    提交代码

--- web/controller/src/main/java/com/example/sort/QuickSortStudy.java
+++ web/controller/src/main/java/com/example/sort/QuickSortStudy.java
@@ -1,45 +1,48 @@
 package com.example.sort;
 
 /**
- * 快速排序
+ * quick sort study record
  *
  * @author lazy cat
- * @since 2020-04-02
- */
-public class QuickSort {
+ * @since 2020-04-18
+ **/
+public class QuickSortStudy {
 
     private static int[] sort(int[] arr, int low, int high) {
         int l = low;
         int h = high;
-        int pivot = arr[low];
+        int pivot = arr[l];
         while (l < h) {
-            while (l < h && arr[h] >= pivot)
+            while (l < h && arr[h] >= pivot) {
                 h--;
+            }
             if (l < h) {
                 int temp = arr[h];
                 arr[h] = arr[l];
                 arr[l] = temp;
                 l++;
             }
-            while (l < h && arr[l] <= pivot)
+            while (l < h && arr[l] <= pivot) {
                 l++;
+            }
             if (l < h) {
-                int temp = arr[h];
-                arr[h] = arr[l];
-                arr[l] = temp;
+                int temp = arr[l];
+                arr[l] = arr[h];
+                arr[h] = temp;
                 h--;
             }
         }
-        if (l > low) sort(arr, low, l - 1);
-        if (h < high) sort(arr, l + 1, high);
+        if (l > low)
+            sort(arr, low, l - 1);
+        if (h < high)
+            sort(arr, l + 1, high);
         return arr;
     }
 
     public static void main(String[] args) {
-
         int[] a6 = {6, 9, 7, 4, 5, 8, 3, 2, 1};
         System.out.println("快速排序算法");
-        int[] b6 = QuickSort.sort(a6, 0, 8);
+        int[] b6 = QuickSortStudy.sort(a6, 0, 8);
         for (int i : b6) {
             System.out.print(i + " ");
         }
