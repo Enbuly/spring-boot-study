e3cf6cb7c4e452272d172a41b83303c434995dd4
zhangzhenyan <120157229@qq.com>
2020-05-03 20:20:57 +0800

    提交代码

---
+++ web/controller/src/main/java/com/example/java8/juc/ProducerConsumer.java
@@ -0,0 +1,55 @@
+package com.example.java8.juc;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * BlockingQueue解决生产者消费者问题
+ *
+ * @author lazy cat
+ * @since 2020-05-03
+ **/
+public class ProducerConsumer {
+
+    private static BlockingQueue<String> queue = new ArrayBlockingQueue<>(5);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            Producer producer = new Producer();
+            producer.start();
+        }
+        for (int i = 0; i < 5; i++) {
+            Consumer consumer = new Consumer();
+            consumer.start();
+        }
+        for (int i = 0; i < 3; i++) {
+            Producer producer = new Producer();
+            producer.start();
+        }
+    }
+
+    private static class Producer extends Thread {
+        @Override
+        public void run() {
+            try {
+                queue.put("product");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("produce..");
+        }
+    }
+
+    private static class Consumer extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                String product = queue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("consume..");
+        }
+    }
+}
