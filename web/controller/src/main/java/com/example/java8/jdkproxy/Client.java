148929c752a80443b0f2158cad2eda10d51c8c48
zhangzhenyan <120157229@qq.com>
2020-05-10 11:05:14 +0800

    提交代码

---
+++ web/controller/src/main/java/com/example/java8/jdkproxy/Client.java
@@ -0,0 +1,34 @@
+package com.example.java8.jdkproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * demo test
+ *
+ * @author lazy cat
+ * @since 2020-05-10
+ **/
+public class Client {
+
+    public static void main(String[] args) {
+
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+
+        //获取对应的 ClassLoader
+        ClassLoader classLoader = userServiceImpl.getClass().getClassLoader();
+
+        //获取所有接口的Class，这里的UserServiceImpl只实现了一个接口UserService，
+        Class[] interfaces = userServiceImpl.getClass().getInterfaces();
+
+        //创建一个将传给代理类的调用请求处理器，处理所有的代理对象上的方法调用
+        //这里创建的是一个自定义的日志处理器，须传入实际的执行对象 userServiceImpl
+        InvocationHandler logHandler = new LogHandler(userServiceImpl);
+
+        UserService proxy = (UserService) Proxy.newProxyInstance(classLoader, interfaces, logHandler);
+
+        // 调用代理的方法
+        proxy.select();
+        proxy.update();
+    }
+}
---
+++ web/controller/src/main/java/com/example/java8/jdkproxy/LogHandler.java
@@ -0,0 +1,37 @@
+package com.example.java8.jdkproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Date;
+
+/**
+ * InvocationHandler
+ *
+ * @author lazy cat
+ * @since 2020-05-10
+ **/
+public class LogHandler implements InvocationHandler {
+
+    //被代理的对象，实际的方法执行者
+    private Object target;
+
+    LogHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        before();
+        Object result = method.invoke(target, args);
+        after();
+        return result;
+    }
+
+    private void before() {
+        System.out.println(String.format("log start time [%s] ", new Date()));
+    }
+
+    private void after() {
+        System.out.println(String.format("log end time [%s] ", new Date()));
+    }
+}
