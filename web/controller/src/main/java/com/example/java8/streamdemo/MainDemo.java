3643367374687f4a858681e9f29b0a6ff673cd5d
zhangzhenyan <120157229@qq.com>
2020-05-19 10:34:17 +0800

    提交代码

--- web/controller/src/main/java/com/example/java8/streamdemo/MainDemo.java
+++ web/controller/src/main/java/com/example/java8/streamdemo/MainDemo.java
@@ -14,6 +14,10 @@ public class MainDemo {
 
     //filter过滤数据
     //map函数的作用就是针对管道流中的每一个数据元素进行转换操作。
+    //limit方法传入一个整数n，用于截取管道中的前n个元素
+    //skip方法与limit方法的使用相反，用于跳过前n个元素，截取从n到末尾的元素
+    //Distinct元素去重
+    //Sorted排序
     public static void main(String[] args) {
         Employee e1 = new Employee(1, 23, "M", "Rick", "Beethovan");
         Employee e2 = new Employee(2, 13, "F", "Martina", "Hengis");
@@ -46,12 +50,14 @@ public class MainDemo {
 
         //使用谓语
         List<Employee> list = employees.stream()
-                .parallel()
                 .filter(Employee.idMaxFour.negate())
                 .peek(e -> {
                     e.setAge(e.getAge() + 1);
                     e.setGender(e.getGender().equals("M") ? "male" : "female");
-                }).collect(Collectors.toList());
+                })
+                .distinct()
+                .limit(5)
+                .collect(Collectors.toList());
 
         for (Employee employee : list) {
             System.out.println(employee.toString());
